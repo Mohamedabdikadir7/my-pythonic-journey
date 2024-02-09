6d2769c519853ecf85ea58facdd83aaa887218d7
Mohamedabdikadir7 <141272771+Mohamedabdikadir7@users.noreply.github.com>
2024-02-09 23:17:31 +0300

    learned tuples

---
+++ tuple.py
@@ -0,0 +1,14 @@
+#Tuples allow you to do just that. Python refers to values that cannot
+#change as immutable, and an immutable list is called a tuple.
+
+# syntax 
+
+#my_tuple = (item1, item2, item3, ...)
+
+# Access Tuple Items
+
+mylit = ("apple" , "mango" , "juice" )
+print(mylit[1])
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-5:-1])
