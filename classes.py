47619134360a7f78cb3e51cf753d0c59f953e123
Mohamedabdikadir7 <141272771+Mohamedabdikadir7@users.noreply.github.com>
2024-03-14 23:08:41 +0300

    added some content

---
+++ classes.py
@@ -0,0 +1,12 @@
+### 
+#Defining a Class:
+
+#Use the class keyword followed by the class name.
+#Define methods (functions) within the class.
+# example
+class MyClass:
+    def __init__(self, name):
+        self.name = name
+
+    def greet(self):
+        print("Hello, " + self.name + "!")
